ff7679e7f7890b7a06c216c9830900e179fa52a9
LUIZ CARLOS PEDRO <luiz_c_pedro@estudante.sc.senai.br>
2025-02-18 19:25:11 -0300

    teste commit

--- src/atividade01.ts
+++ src/atividade01.ts
@@ -1,3 +1,18 @@
+import leia from 'readline-sync'
+
 export default function atividade01() {
-  console.log('Atividade 01');
+  // Questão 01
+  // Escreva um programa que solicite ao usuário uma nota de 0 a 100. 
+  // O programa deve imprimir o conceito correspondente com base na tabela abaixo:
+
+  // Nota entre 90 e 100: "A"
+  // Nota entre 80 e 89: "B"
+  // Nota entre 70 e 79: "C"
+  // Nota entre 60 e 69: "D"
+  // Nota abaixo de 60: "F"
+  // Se a nota inserida for inválida (menor que 0 ou maior que 100), o programa deve imprimir uma mensagem de erro.
+
+
+
+
 }
